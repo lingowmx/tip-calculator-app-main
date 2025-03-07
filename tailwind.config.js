54e2b2df9328d3e36ac3c0e3f131e682f9561e43
lingowmx <clingow@hotmail.com>
2025-03-07 10:02:06 +0100

    colors and fontfamily ok

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,20 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors:{
+        Strongcyan: 'hsl(172, 67%, 45%)',
+        Verydarkcyan: 'hsl(183, 100%, 15%)',
+        Darkgrayishcyan: 'hsl(186, 14%, 43%)',
+        Grayishcyan: 'hsl(184, 14%, 56%)',
+        Lightgrayishcyan: 'hsl(185, 41%, 84%)',
+        Verylightgrayishcyan: 'hsl(189, 41%, 97%)',
+        White: 'hsl(0, 0%, 100%)',
+      },
+      fontFamily:{
+        spaceMono: ["Space-Mono", "Sans-Serif"]
+      }
+    },
   },
   plugins: [],
 }
